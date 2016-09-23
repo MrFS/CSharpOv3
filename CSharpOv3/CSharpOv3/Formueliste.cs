9aa68a5cc5abe53d9b871c8e75e3045f7aa1969e
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-09-23 14:07:15 +0200

    finito

--- CSharpOv3/CSharpOv3/Formueliste.cs
+++ CSharpOv3/CSharpOv3/Formueliste.cs
@@ -13,25 +13,14 @@ namespace CSharpOv3
                                                  new Person("Carl Chica", 200000) }; //Chica Chica Boom Boom
 
         Person[] noobArray = new Person[] { new Person("Arne Aas", 0),
-                                             new Person("Berit Bø", 4300900),
-                                             new Person("Carl Chica", 200000) };
+                                            new Person("Berit Bø", 4300900),
+                                            new Person("Carl Chica", 200000) };
 
         public bool registrerNyPerson(Person p)
         {
-            //try
-            //{
-            //    pers.Add(p);
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //    return false;
-            //}
-
-            try
+            try //List
             {
-                Array.Resize<Person>(ref noobArray, noobArray.Length + 1);
-                noobArray[noobArray.Length - 1] = new Person(p.Navn, p.Formue);
+                pers.Add(p);
             }
             catch (Exception e)
             {
@@ -39,62 +28,86 @@ namespace CSharpOv3
                 return false;
             }
 
+            //try //Array
+            //{
+            //    Array.Resize<Person>(ref noobArray, noobArray.Length + 1);
+            //    noobArray[noobArray.Length - 1] = new Person(p.Navn, p.Formue);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //    return false;
+            //}
+
             return true;
         }
 
         public override string ToString()
         {
-            foreach (Person item in noobArray)
-            {
-                Console.WriteLine(item.ToString());
-            }
-
-
-
-            //foreach (Person item in pers)
+            //foreach (Person item in noobArray)
             //{
-            //    Console.WriteLine(item.Navn + " " + item.Formue);
+            //    Console.WriteLine(item.ToString());
             //}
+
+
+
+            foreach (Person item in pers)
+            {
+                Console.WriteLine(item.Navn + " " + item.Formue);
+            }
 
             return "\n";
         }
 
         public Person getRikest()
         {
-            //int x = 0, y = 0;
-            //for (int i = 0; i < pers.Count; i++)
-            //{
-            //    if (pers.ElementAt(i).Formue > y)
-            //        y = pers.ElementAt(i).Formue;
-            //        x = i;
-            //}
-            //return pers.ElementAt(x);
-
+            //Liste
             int x = 0, y = 0;
-            for (int i = 0; i < noobArray.Length; i++)
+            for (int i = 0; i < pers.Count; i++)
             {
-                if (noobArray[i].Formue > y)
+                if (pers.ElementAt(i).Formue > y)
                 {
-                    y = noobArray[i].Formue;
+                    y = pers.ElementAt(i).Formue;
                     x = i;
                 }
             }
+            return pers.ElementAt(x);
 
-            return noobArray[x];
+
+            //Array
+            //int x = 0, y = 0;
+            //for (int i = 0; i < noobArray.Length; i++)
+            //{
+            //    if (noobArray[i].Formue > y)
+            //    {
+            //        y = noobArray[i].Formue;
+            //        x = i;
+            //    }
+            //}
+
+            //return noobArray[x];
         }
 
         public void Sorter()
         {
-            Person[] kopi = noobArray;
+            //Person[] kopi = noobArray;
 
-            Array.Sort(kopi, delegate(Person x, Person y) { return y.Formue.CompareTo(x.Formue); });
+            //Array.Sort(kopi, delegate(Person x, Person y) { return y.Formue.CompareTo(x.Formue); });
+
+            //foreach (Person item in kopi)
+            //{
+            //    Console.WriteLine(item.ToString());
+            //}
+
+            List<Person> kopi = pers;
+
+            kopi.Sort(delegate (Person x, Person y) { return y.Formue.CompareTo(x.Formue); });
 
             foreach (Person item in kopi)
             {
                 Console.WriteLine(item.ToString());
             }
-
-
         }
+        
     }
 }
--- CSharpOv3/CSharpOv3/Program.cs
+++ CSharpOv3/CSharpOv3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +28,78 @@ namespace CSharpOv3
 
             lst.Sorter();
 
+            Console.WriteLine("Trykk en tast for å fortsette");
+
             Console.ReadKey();
+
+            Console.WriteLine("____________________________________________________");
+
+
+            do
+            {
+                try
+                {
+
+                    do
+                    {
+                        Console.Write("Skriv navn: ");
+                        string x = Console.ReadLine();
+                        int y = 0;
+                        Console.Write("Skriv Formue: ");
+                        if (Int32.TryParse(Console.ReadLine(), out y))
+                        {
+                            Person u = new Person(x, y);
+
+                            if (lst.registrerNyPerson(u))
+                            {
+                                Console.WriteLine("Du har registrert en ny person : " + u.ToString());
+                            }
+                            else
+                            {
+                                Console.WriteLine("En feil skjedde ved registrering av ny person.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ugyldig Formue");
+                        }
+
+                        Console.WriteLine("Registrere ny person? Y/N");
+
+                        
+                        if (Console.ReadKey().Key == ConsoleKey.N)
+                        {
+                            Console.WriteLine("\n");
+                            break;
+                        }
+
+                    } while (true);
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                    if (Debugger.IsAttached)
+                        Debug.WriteLine(e.Message);
+                }
+
+                Console.WriteLine("____________________________________________________");
+
+                Console.WriteLine("\n" + 
+                                  "Printer nå innholdet fra listen/tabellen\n" + 
+                                  "Sortering med personen som har størst formue først\n");
+                lst.Sorter();
+
+                Console.WriteLine("\n" + 
+                                  "Finner den rikeste personen og printer ut vedkommende\n\n" +
+                                  lst.getRikest().ToString());
+
+
+                
+
+
+            } while (Console.ReadKey().Key != ConsoleKey.Q);
         }
     }
 }
